81a9a2a3a2ea25441032ed82ab042472b2d1e404
Andrew Dinh <83081141+Banhkun@users.noreply.github.com>
2026-05-07 15:32:42 +0700

    Update runmyjobs-tab-nav.user.js

--- runmyjobs-tab-nav.user.js
+++ runmyjobs-tab-nav.user.js
@@ -4,7 +4,7 @@
 // @version      1.3
 // @description  Tab/Shift+Tab navigation, context menu shortcuts, Ctrl+S/E/Q
 // @author       You
-// @match        https://runmyjobs-*.emea.bosch.com/redwood/ui*
+// @include      https://runmyjobs-*.emea.bosch.com/redwood/ui*
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
