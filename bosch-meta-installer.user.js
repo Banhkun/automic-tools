cd8588a6e2af6ff6567d59cb301c755b975bfdd6
Andrew Dinh <83081141+Banhkun@users.noreply.github.com>
2026-05-20 11:40:06 +0700

    Update bosch-meta-installer.user.js

--- bosch-meta-installer.user.js
+++ bosch-meta-installer.user.js
@@ -35,10 +35,6 @@ const ROUTES = [
     src: `${BASE}/Detail-Page-Tools.user.js`,
   },
     {
-    match: () => href.includes('emea.bosch.com/redwood/ui'),
-    src: `${BASE}/runmyjobs-batch-updater.user.js`,
-  },
-    {
     match: () => href.includes('rb-wam.bosch.com') && href.includes('f?p=100:44'),
     src: `${BASE}/Mal-column-filler.user.js`,
   },
