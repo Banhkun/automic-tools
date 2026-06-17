b050caa17ee8be24568c68a2dbeb9019575b4b22
Andrew Dinh <83081141+Banhkun@users.noreply.github.com>
2026-06-17 11:19:05 +0700

    Update bosch-meta-installer.user.js

--- bosch-meta-installer.user.js
+++ bosch-meta-installer.user.js
@@ -31,7 +31,7 @@ const CACHE_TTL_MS = 6 * 60 * 60 * 1000;
 // Each entry: [ match(), script file ]
 const ROUTES = [
   {
-    match: () => href.includes("apps-p-p1-outsystems.de.bosch.com/ASPortal"),
+    match: () => href.includes("apps-p-p1-outsystems.de.bosch.com/ASPortal/RequestDetail"),
     src: `${BASE}/Detail-Page-Tools.user.js`,
   },
   {
